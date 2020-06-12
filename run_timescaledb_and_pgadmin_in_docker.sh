11ab22f18a98f0d9fecc6939a33502e20df8d4b5
tuenut <unnetu@gmail.com>
2020-06-13 01:01:35 +0300

    fix

--- run_timescaledb_and_pgadmin_in_docker.sh
+++ run_timescaledb_and_pgadmin_in_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 read -p 'Enter containers name prefix: ' container_name;
+read -sp 'Enter password for timescaleDB: ' tsdb_password;
 read -p 'Enter email for pgAdmin: ' email;
 read -sp 'Enter password for pgAdmin: ' password;
 
-docker run -d --name "$container_name.timescaleDB" -p 5432:5432 timescale/timescaledb:latest-pg12;
+docker run -d --name "$container_name.timescaleDB" -p 5432:5432 -e "POSTGRES_PASSWORD=$tsdb_password" timescale/timescaledb:latest-pg12;
 docker run -d --name "$container_name.pgAdmin" -p 5050:80 -e "PGADMIN_DEFAULT_EMAIL=$email" -e "PGADMIN_DEFAULT_PASSWORD=$password" dpage/pgadmin4;
 
 exit 0;
